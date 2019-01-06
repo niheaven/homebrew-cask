a02e49ef97435854e8cfaefc827f3fa9e18cb59b
Hsiao-nan Cheung <niheaven@gmail.com>
2019-01-06 14:59:11 +0800

    Update bwana to 2.8 and add appcast

--- Casks/bwana.rb
+++ Casks/bwana.rb
@@ -1,8 +1,9 @@
 cask 'bwana' do
-  version :latest
-  sha256 :no_check
+  version '2.8'
+  sha256 'c080fdda304dcd4f30758530dd39b01f48bdf34b6914dab770271a01e86b9223'
 
   url 'https://www.bruji.com/bwana/bwana.dmg'
+  appcast 'https://www.bruji.com/bwana/new.html'
   name 'Bwana'
   homepage 'https://www.bruji.com/bwana/'
 
